dee34767e29d619cf41f42da4a60c6290d74793f
Hoàng Phạm Đức <hoang23577@gmail.com>
2024-01-31 14:16:17 +0700

    Buoi_6_Static

---
+++ Buoi_6_Static/ThuVien.cs
@@ -0,0 +1,11 @@
+﻿namespace Buoi_6_Static
+{
+    internal static class ThuVien
+    {
+        public static int namSinh = 1;
+        public static void InThongTin()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
